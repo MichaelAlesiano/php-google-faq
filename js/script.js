dc7d42fa972db9b0e29c6f9b63bb94373429c660
Michael <michael.alesiano@gmail.com>
2021-03-02 14:36:08 +0100

    First push

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    headerCounter: 4,
+    headerNav: ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologie', 'Domande Frequenti'],
+    footerNav: ['Google','Tutto su Google','Privacy','Termini']
+  }
+});
